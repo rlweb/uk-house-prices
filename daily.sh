5809d749e9ab2548c7d021b3cd3d0ff4ed763750
Rhys Laval <rhyslaval@gmail.com>
2020-12-12 20:46:37 +0000

    Github Aciton - Mock a new transaction

--- daily.sh
+++ daily.sh
@@ -2,6 +2,12 @@
 
 curl -L http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-monthly-update-new-version.csv | sed 's/ 00\:00//g' >> pp-monthly-update-new-version.csv
 
+gsutil hash gs://uk-house-price-data/pp-monthly-update-new-version.csv ./pp-monthly-update-new-version.csv
+echo $?
+echo $?
+echo $?
+echo $?
+
 if gsutil hash gs://uk-house-price-data/pp-monthly-update-new-version.csv ./pp-monthly-update-new-version.csv; then
   gsutil mv pp-monthly-update-new-version.csv gs://uk-house-price-data/pp-monthly-update-new-version.csv
   bq load uk_house_price_data.all_data gs://uk-house-price-data/pp-monthly-update-new-version.csv
